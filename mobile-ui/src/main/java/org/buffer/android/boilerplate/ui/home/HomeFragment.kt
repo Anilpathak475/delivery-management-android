642c1ba00022e5e1cb87bd4260f3020e6e894d85
suhas mandrawadkar <suhas.sm@gmail.com>
2018-10-21 18:32:38 +1100

    Adds FAB

--- mobile-ui/src/main/java/org/buffer/android/boilerplate/ui/home/HomeFragment.kt
+++ mobile-ui/src/main/java/org/buffer/android/boilerplate/ui/home/HomeFragment.kt
@@ -1,13 +1,18 @@
 package org.buffer.android.boilerplate.ui.home
 
 
+import android.animation.Animator
 import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewAnimationUtils
 import android.view.ViewGroup
-
+import android.view.animation.RotateAnimation
+import kotlinx.android.synthetic.main.fragment_home.*
 import org.buffer.android.boilerplate.ui.R
+
 
 class HomeFragment : Fragment() {
 
@@ -15,6 +20,68 @@ class HomeFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initListeners()
+    }
+
+    private fun initListeners() {
+        (floatingActionButton as FloatingActionButton).setOnClickListener {
+           if (toolbar.visibility == View.VISIBLE) {
+               hideToolbar()
+           } else {
+               revealToolbar()
+           }
+        }
+    }
+
+    private fun revealToolbar() {
+
+        val x = floatingActionButton.x + floatingActionButton.width/2
+        val y = floatingActionButton.height/2
+
+        val startRadius = 0
+        val endRadius = (Math.hypot(toolbar.width.toDouble(), toolbar.height.toDouble()))
+
+        val animator = ViewAnimationUtils.createCircularReveal(toolbar, x.toInt(), y, startRadius.toFloat(), endRadius.toFloat())
+        toolbar.visibility = View.VISIBLE
+        animator.start()
+
+        floatingActionButton.rotation = 45f
+    }
+
+    private fun hideToolbar() {
+
+        val x = floatingActionButton.x + floatingActionButton.width/2
+        val y = floatingActionButton.height/2
+
+        val startRadius = Math.max(toolbar.width, toolbar.height)
+        val endRadius = 0
+
+        val animator = ViewAnimationUtils.createCircularReveal(toolbar, x.toInt(), y, startRadius.toFloat(), endRadius.toFloat())
+        animator.start()
+
+        floatingActionButton.rotation = 0f
+
+        animator.addListener(object : Animator.AnimatorListener {
+            override fun onAnimationRepeat(animation: Animator?) {
+
+            }
+
+            override fun onAnimationEnd(animation: Animator?) {
+                toolbar.visibility = View.INVISIBLE
+            }
+
+            override fun onAnimationCancel(animation: Animator?) {
+
+            }
+
+            override fun onAnimationStart(animation: Animator?) {
+
+            }
+        })
     }
 
 
