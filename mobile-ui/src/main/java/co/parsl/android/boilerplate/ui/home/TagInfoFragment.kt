4b483ebab403b50544d1b8dcc43517d125843909
anilpathak <anilpathak475@gmail.com>
2018-11-29 11:01:15 +0530

    adds button for handover process

--- mobile-ui/src/main/java/co/parsl/android/boilerplate/ui/home/TagInfoFragment.kt
+++ mobile-ui/src/main/java/co/parsl/android/boilerplate/ui/home/TagInfoFragment.kt
@@ -3,12 +3,23 @@ package co.parsl.android.boilerplate.ui.home
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import co.parsl.android.boilerplate.ui.shared.AppHelper
 
 import co.parsl.android.ui.R
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoDevice
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUserSession
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.continuations.AuthenticationContinuation
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.continuations.ChallengeContinuation
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.continuations.MultiFactorAuthenticationContinuation
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.handlers.AuthenticationHandler
+import kotlinx.android.synthetic.main.fragment_login.*
 import kotlinx.android.synthetic.main.fragment_tag_info.*
+import kotlinx.android.synthetic.main.fragment_welcome.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -37,5 +48,16 @@ class TagInfoFragment : Fragment() {
         tagId.text = uniqueIdValue
         infoUrl.text = assignedUrlValue
         tagStatus.text = status
+    }
+
+    private fun initListeners() {
+        sendButton.setOnClickListener {   }
+        receiveButton.setOnClickListener {   }
+        sendConfirm.setOnClickListener {   }
+        receiveConfirmButton.setOnClickListener {   }
+    }
+
+    private fun invokeCognitoLogin() {
+       // AppHelper.getPool().
     }
 }
