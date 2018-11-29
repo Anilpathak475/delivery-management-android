b285717cee88b6a18eaf5f0f647c2398fad29c9d
suhas mandrawadkar <suhas.sm@gmail.com>
2018-11-29 18:25:52 +1100

    adds api services

--- remote/src/main/java/co/parsl/android/boilerplate/remote/ParslService.kt
+++ remote/src/main/java/co/parsl/android/boilerplate/remote/ParslService.kt
@@ -5,6 +5,7 @@ import co.parsl.android.boilerplate.remote.model.TagInfoPojo
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface ParslService {
@@ -16,7 +17,49 @@ interface ParslService {
     ) : Call<TagInfoPojo>
 
     data class GetTagInfoResponse(val tagInfo: TagInfoModel)
+
+    @GET("secured/tags/{tagId}/ledger")
+    fun getLedger(@Path("tagId") tagId : String,
+                      @Header("Authorization") token : String) : Call<Void>
+
+    /*
+    {
+    "elements": [
+        {
+            "user": "4b8357bb-f817-4546-a61a-0f9cfbcfc9e6",
+            "transactionTs": "2018-11-28 11:43:45.796",
+            "action": "TAKEOVER"
+        },
+        {
+            "user": "4b8357bb-f817-4546-a61a-0f9cfbcfc9e6",
+            "transactionTs": "2018-11-28 11:43:45.823",
+            "action": "HANDOVER_SEND"
+        }
+    ],
+    "totalPages": 1,
+    "totalElements": 2
+    }
+     */
+
+    @POST("secured/tags/{tagId}/handover?action={handoverAction}")
+    fun postHandoverAction(@Path("tagId") tagId : String,
+                           @Path("handoverAction") handoverAction: String,
+                           @Header("Authorization") token : String) : Call<Void>
+
 }
+
+// EditText TagId
+// Token Shared Prefs
+
+//Buttons
+//SEND
+//RECIEVE
+//SEND_CONFIRM
+//RECEIVE_CONFIRM
+
+//LEDGER
+
+//TAKEOVER, HANDOVER_SEND
 
 //Get products - Select Product
 //Get product batches by product
